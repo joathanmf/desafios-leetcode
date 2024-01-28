123f77db3aa15347fc5859d21f626e576efcbfef
Joathan Mareto <joathanmareto@gmail.com>
2024-01-27 22:59:57 -0300

    inicial, 2 desafios

---
+++ first_missing_positive.rs
@@ -0,0 +1,57 @@
+fn first_missing_positive_v1(mut nums: Vec<i32>) -> i32 {
+    let mut i = 0;
+    let mut n = 1;
+
+    nums.sort();
+
+    while i < nums.len() {
+        if nums[i] > 0 {
+            if nums[i] != n {
+                return n;
+            }
+
+            n += 1;
+        }
+
+        i += 1;
+    }
+
+    return n;
+}
+
+// Leetcode
+fn first_missing_positive_v2(mut nums: Vec<i32>) -> i32 {
+    let mut i = 0;
+    let len = nums.len();
+    
+    while i < len {
+        let cur = nums[i];
+
+        if cur > 0 && cur <= len as i32 {
+            if nums[(cur - 1) as usize] != cur {
+                let aux = cur;
+                nums[i] = nums[(cur - 1) as usize];
+                nums[(cur - 1) as usize] = aux;
+    
+                continue;
+            }
+        }
+
+        i += 1;
+    }
+
+    for i in 0..len {
+        if nums[i] != (i + 1) as i32 {
+            return (i + 1) as i32;
+        }
+    }
+
+    return (len + 1) as i32;
+}
+
+fn main() {
+    let nums = vec![3, 4, -1, 1, 2, 5, 6, -2, 7];
+
+    println!("Primeiro positivo faltando (v1): {}", first_missing_positive_v1(nums.clone()));
+    println!("Primeiro positivo faltando (v2): {}", first_missing_positive_v2(nums));
+}
---
+++ first_unique_character_in_a_string.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+fn first_uniq_char(s: String) -> i32 {
+    let mut hash = HashMap::<char, usize>::new();
+
+    for ch in s.chars() {
+        hash.entry(ch).and_modify(|c| *c += 1).or_insert(1);
+    }
+
+    for (i, ch) in s.chars().enumerate() {
+        let value = hash.get(&ch);
+
+        match value {
+            Some(v) => {
+                if *v as i32 == 1 {
+                    return i as i32;
+                }
+            }
+            None => {}
+        }
+    }
+
+    return -1;
+}
+
+fn main() {
+    let s = "bbbbbbbbbbbbcd".to_string();
+    let res = first_uniq_char(s);
+
+    println!("Resposta: {}", res);
+}
+
